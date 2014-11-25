9ca134067ec8b05adb609c9aa9513793982203d3
Jennifer Maurer <maurerjc@uw.edu>
2014-11-24 18:51:06 -0800

    created map, fetch list of traffic cameras, add them as markers on map, pan map to clicked marker, open InfoWindow of clicked img, filter search

--- js/app.js
+++ js/app.js
@@ -3,11 +3,66 @@
 
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
+//create the map, fetch  the list of traffic cameras, and add them as markers on the map
+$(document).ready(function() {
+	var mapElem = document.getElementById('map');
+	var center = {
+		lat: 47.6,
+		lng: -122.3
+	};
+	var map = new google.maps.Map(mapElem, {
+		center: center,
+		zoom: 12
+	});
 
+	var infoWindow = new google.maps.InfoWindow();
+
+	$.getJSON('https://data.seattle.gov/resource/65fc-btcc.json')
+		.done(function(data) {
+			//cameras = data; //save the data
+			data.forEach(function(camera) {//for each camera
+				var marker = new google.maps.Marker({//create a marker at the camera's location
+					position: {
+						lat: Number(camera.location.latitude),
+						lng: Number(camera.location.longitude)
+					},
+					map: map //assign marker to this map
+				});
+				google.maps.event.addListener(marker, 'click', function() { //when click a marker
+					//"this" refers to map (element that raised event)
+					var latLng = this.getPosition();
+					//pan map so clicked marker is in center
+					map.panTo(latLng);
+					//open an InfoWindow that displays the latest camera image + label
+					var labelImg = '<p>' + camera.cameralabel + '</p>';
+					labelImg += '<img src=' + camera.imageurl.url + '>';
+					infoWindow.setContent(labelImg);
+					infoWindow.open(map, this)//map open on, and anchor to refer to marker
+				})
+				//close InfoWindow when user clicks on map
+				google.maps.event.addListener(map, 'click', function() {
+					infoWindow.close();
+				});
+
+				//filter markers when type in search box
+				$('#search').bind('search keyup', function() {
+					var markerLabel = camera.cameralabel;
+					var input = $('#search').val().toLowerCase();
+					if (markerLabel.toLowerCase().indexOf(input) >= 0) {
+						marker.setMap(map);
+					} else {
+						marker.setMap(null);
+					}
+				})
+			});
+
+		})
+		.fail(function(error) {
+			alert("There was an error.");
+			console.log(error);
+		})
+		.always(function() {
+			$('#ajax-loader').fadeOut();
+		});
+
+});
